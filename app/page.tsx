f79115c36e1b8b0d0c96bc7b505b46672d270da8
Klimova Alina <alinaklimova82@gmail.com>
2024-08-10 22:17:19 +0600

    Finished chapters 4-7. Created dashboard routes using file system routing. Created a nested layout for use on different pages. I used next/link. Showed the active link using usePathname(). Created and linked my project with a Postgres database. Filled the database with initial data.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
         <div
-            // className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
             className={styles.shape}
         />
       </div>
